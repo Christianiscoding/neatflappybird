70595294b2ed49c3191cd144468f6fc4fcd6514e
Christian <ChristianSchmitz.idk@gmail.com>
2022-06-18 01:04:05 +0200

    pictures

--- FlappyBirdNeatIntro.py
+++ FlappyBirdNeatIntro.py
@@ -7,4 +7,7 @@ import random
 WIN_WIDTH = 600
 WIN_HEIGHT = 800
 
-Bird_imgs =
+pipe_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")).convert_alpha())
+bg_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs","bg.png")).convert_alpha(), (600, 900))
+bird_images = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird" + str(x) + ".png"))) for x in range(1,4)]
+base_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")).convert_alpha())
